97fe033b107313b024edb0a3c46fb44aa7fad42a
liviusgrosu <livius.grosu@ryerson.ca>
2022-06-15 18:48:36 -0400

    Checked correctly when a player is grounded and applied gravity when in the air

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,34 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // General Movement
+    private float _horizontalMovement;
+    private float _verticalMovement;
+    private Vector3 _moveDirection;
+
+    // Movement Speeds
     [SerializeField]
     private float _walkSpeed;
     [SerializeField]
     private float _runSpeed;
+    [SerializeField]
+    private float _jumpSpeed;
+    [SerializeField] 
+    private float _fallRate;
 
+    // Movement States
     private bool _isRunning;
+    private bool _isJumping;
 
-    private float _horizontalMovement;
-    private float _verticalMovement;
-    private Vector3 _moveDirection;
     private Rigidbody _rigidBody;
+    private CapsuleCollider _collider;
+    private float _distanceToFeet;
+    private float _colliderRadius = 0.1f;
+    private float _maxDistance = 1.0f;
 
 
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody>();
+        _collider = GetComponent<CapsuleCollider>();
+        _distanceToFeet = _collider.height / 2f;
     }
 
     void Update()
     {
+        IsGrounded();
+
         _horizontalMovement = Input.GetAxis("Horizontal");
         _verticalMovement = Input.GetAxis("Vertical");
 
-        Debug.Log($"{_horizontalMovement}, {_verticalMovement}");
-
         _moveDirection = (_horizontalMovement * transform.right + _verticalMovement * transform.forward).normalized;
 
-        
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            Jump();
+        }
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
@@ -45,8 +63,27 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float movementSpeed = _isRunning ? _runSpeed : _walkSpeed; 
-        _rigidBody.velocity = _moveDirection * movementSpeed * Time.fixedDeltaTime * 100f;
+        float movementSpeed = _isRunning ? _runSpeed : _walkSpeed;
+
+        Vector3 YAxisGravity = new Vector3(0, _rigidBody.velocity.y - _fallRate, 0);
+
+        _rigidBody.velocity = (_moveDirection * movementSpeed * Time.fixedDeltaTime * 100f) + YAxisGravity;
     }
 
+    private void Jump()
+    {
+        _rigidBody.AddForce(new Vector3(0, _jumpSpeed, 0), ForceMode.Impulse);
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit hitInfo;
+        if (Physics.SphereCast(transform.position, _colliderRadius, Vector3.down, out hitInfo, _distanceToFeet))
+        {
+            // Reset jump state
+            return true;
+        }
+
+        return false;
+    }
 }
