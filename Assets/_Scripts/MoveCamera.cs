bb3f555a0a674128f39bccb1462bdd5a715a085e
liviusgrosu <livius.grosu@ryerson.ca>
2022-06-17 12:17:00 -0400

    Added back the camera controls

---
+++ Assets/_Scripts/MoveCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField] Transform cameraPosition = null;
+
+    void Update()
+    {
+        transform.position = cameraPosition.position;
+    }
+}
