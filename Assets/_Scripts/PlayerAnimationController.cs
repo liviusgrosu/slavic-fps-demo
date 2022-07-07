a18064c07e122238dc7a69c0d6913d48dd7be92e
liviusgrosu <livius.grosu@ryerson.ca>
2022-07-07 18:03:52 -0400

    Added a simple idling and vaulting animation

---
+++ Assets/_Scripts/PlayerAnimationController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+public class PlayerAnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator _playerArms;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            _playerArms.SetTrigger("Vault");
+        }
+    }
+}
